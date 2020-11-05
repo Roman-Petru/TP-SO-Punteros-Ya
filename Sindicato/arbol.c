9528137c0d6c2592868888aaf74d9be6d38c11aa
Silvia Monica Gutierrez <silviamgutierrez@live.com.ar>
2020-11-04 22:09:59 -0300

    arbol y otros

--- Sindicato/arbol.c
+++ Sindicato/arbol.c
@@ -1,5 +1,16 @@
 #include arbol.h
 
-//ESTRUCTURA DEL ARBOL DE DIRECTORIO
 
-//PUNTO DE MONTAJE
+//ESTRUCTURA DEL ARBOL DE DIRECTORIO
+NodoArbol* crearNodo(char* nombre) {
+
+	NodoArbol* nodo = malloc(sizeof(NodoArbol));
+	nodo->nombre = string_duplicate(nombre);
+	nodo->hijos = list_create();
+	return nodo;
+}
+
+void agregarNodo(NodoArbol* padre, NodoArbol* hijo) {
+	list_add(padre->hijos, hijo);
+}
+
