b32bd21e1ecf256faf1e64671c2cb5c7c36f4c9d
Roman-Petru <ropetru@hotmail.com>
2020-11-28 02:20:19 -0300

    ips

--- Comanda/comanda.c
+++ Comanda/comanda.c
@@ -8,74 +8,53 @@ t_list* tabla_restaurantes;
 t_list* paginas_en_memoria;
 
 int puntero_a_paginas;
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <netdb.h>
 #include <sys/types.h>
-#include <sys/socket.h>
+#include <ifaddrs.h>
 #include <netinet/in.h>
+#include <string.h>
 #include <arpa/inet.h>
 
-// Returns hostname for the local computer
-void checkHostName(int hostname)
-{
-    if (hostname == -1)
-    {
-        perror("gethostname");
-        exit(1);
-    }
-}
-
-// Returns host information corresponding to host name
-void checkHostEntry(struct hostent * hostentry)
-{
-    if (hostentry == NULL)
-    {
-        perror("gethostbyname");
-        exit(1);
-    }
-}
-
-// Converts space-delimited IPv4 addresses
-// to dotted-decimal format
-void checkIPbuffer(char *IPbuffer)
-{
-    if (NULL == IPbuffer)
-    {
-        perror("inet_ntoa");
-        exit(1);
-    }
-}
-
-
-
 void inicializar_comanda(){
+
 	logger = log_create("comanda.log", "COMANDA", true, LOG_LEVEL_INFO);
 	config = config_create("comanda.config");
 
-    char hostbuffer[256];
-    char *IPbuffer;
-    struct hostent *host_entry;
-    int hostname;
+    struct ifaddrs * ifAddrStruct=NULL;
+    struct ifaddrs * ifa=NULL;
+    void * tmpAddrPtr=NULL;
 
-    // To retrieve hostname
-    hostname = gethostname(hostbuffer, sizeof(hostbuffer));
-    checkHostName(hostname);
+    getifaddrs(&ifAddrStruct);
+    char* IP;
 
-    // To retrieve host information
-    host_entry = gethostbyname(hostbuffer);
-    checkHostEntry(host_entry);
+    for (ifa = ifAddrStruct; ifa != NULL; ifa = ifa->ifa_next) {
+        if (!ifa->ifa_addr) {
+            continue;
+        }
+        if (ifa->ifa_addr->sa_family == AF_INET) { // check it is IP4
+            // is a valid IP4 Address
+            tmpAddrPtr=&((struct sockaddr_in *)ifa->ifa_addr)->sin_addr;
+            char addressBuffer[INET_ADDRSTRLEN];
+            inet_ntop(AF_INET, tmpAddrPtr, addressBuffer, INET_ADDRSTRLEN);
+            if (strcmp("enp0s3", ifa->ifa_name) == 0)
+                   {IP = strdup(addressBuffer);
+                   break;  }
+            printf("%s IP Address %s\n", ifa->ifa_name, addressBuffer);
+        } else if (ifa->ifa_addr->sa_family == AF_INET6) { // check it is IP6
+            // is a valid IP6 Address
+            tmpAddrPtr=&((struct sockaddr_in6 *)ifa->ifa_addr)->sin6_addr;
+            char addressBuffer[INET6_ADDRSTRLEN];
+            inet_ntop(AF_INET6, tmpAddrPtr, addressBuffer, INET6_ADDRSTRLEN);
+            printf("%s IP Address %s\n", ifa->ifa_name, addressBuffer);
+        }
 
-    // To convert an Internet network
-    // address into ASCII string
-    IPbuffer = inet_ntoa(*((struct in_addr*)
-                           host_entry->h_addr_list[0]));
+    }
+    if (ifAddrStruct!=NULL) freeifaddrs(ifAddrStruct);
 
-
+    log_info(logger, "IP: %s", IP);
 	serializacion_inicializar();
-	servidor = servidor_crear(IPbuffer, config_get_string_value(config, "PUERTO_ESCUCHA"));
+	servidor = servidor_crear(IP, config_get_string_value(config, "PUERTO_ESCUCHA"));
 	cargar_interfaz();
 
 	inicializar_memoria_principal();
